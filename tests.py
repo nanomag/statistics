424a5897ff7ab99ccb0fd314eb4c20bfb1a3f594
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-11-30 10:52:39 -0500

    Add fixture for tests and update syntax for using pytest

--- tests.py
+++ tests.py
@@ -1,42 +1,30 @@
-import unittest
+import pytest
 
 from main import DataCapture
 
 
-class TestStats(unittest.TestCase):
-    def test_less(self):
-        capture = DataCapture()
-        capture.add(3)
-        capture.add(9)
-        capture.add(3)
-        capture.add(4)
-        capture.add(6)
+@pytest.fixture
+def data_capture():
+    capture = DataCapture()
+    capture.add(3)
+    capture.add(9)
+    capture.add(3)
+    capture.add(4)
+    capture.add(6)
 
-        stats = capture.build_stats()
-        self.assertEqual(stats.less(4), 2)
-
-    def test_between(self):
-        capture = DataCapture()
-        capture.add(3)
-        capture.add(9)
-        capture.add(3)
-        capture.add(4)
-        capture.add(6)
-
-        stats = capture.build_stats()
-        self.assertEqual(stats.between(3, 6), 4)
-
-    def test_greater(self):
-        capture = DataCapture()
-        capture.add(3)
-        capture.add(9)
-        capture.add(3)
-        capture.add(4)
-        capture.add(6)
-
-        stats = capture.build_stats()
-        self.assertEqual(stats.greater(4), 2)
+    return capture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_less(data_capture):
+    stats = data_capture.build_stats()
+    assert stats.less(4), 2
+
+
+def test_between(data_capture):
+    stats = data_capture.build_stats()
+    assert stats.between(3, 6), 4
+
+
+def test_greater(data_capture):
+    stats = data_capture.build_stats()
+    assert stats.greater(4), 2
