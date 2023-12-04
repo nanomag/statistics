72ce3e744163ee5094604e8fe81e83d733b268c8
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-12-04 15:17:17 -0500

    Add unit tests validating invalid number inputs

--- tests.py
+++ tests.py
@@ -3,6 +3,30 @@ import pytest
 from main import DataCapture
 
 
+def test_capture_valid_numbers():
+    capture = DataCapture()
+    capture.add(1)
+    capture.add(2)
+    capture.add(3)
+    assert len(capture.numbers) == 3
+
+
+def test_capture_valid_string_numbers():
+    capture = DataCapture()
+    capture.add(1)
+    capture.add("2")
+    capture.add("3")
+    assert len(capture.numbers) == 3
+
+
+def test_capture_invalid_string_value():
+    capture = DataCapture()
+    capture.add(1)
+    capture.add(2)
+    capture.add("wrong")
+    assert len(capture.numbers) == 2
+
+
 @pytest.fixture
 def data_capture():
     capture = DataCapture()
@@ -17,14 +41,14 @@ def data_capture():
 
 def test_less(data_capture):
     stats = data_capture.build_stats()
-    assert stats.less(4), 2
+    assert stats.less(4) == 2
 
 
 def test_between(data_capture):
     stats = data_capture.build_stats()
-    assert stats.between(3, 6), 4
+    assert stats.between(3, 6) == 4
 
 
 def test_greater(data_capture):
     stats = data_capture.build_stats()
-    assert stats.greater(4), 2
+    assert stats.greater(4) == 2
